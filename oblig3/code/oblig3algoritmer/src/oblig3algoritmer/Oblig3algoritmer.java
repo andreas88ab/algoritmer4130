9b8b667242de538f23352adf2da9a118a9c66dd3
Andreas Berglihn <andbergl@diamant.ifi.uio.no>
2013-11-15 13:20:14 +0100

    la til filer

--- oblig3/code/oblig3algoritmer/src/oblig3algoritmer/Oblig3algoritmer.java
+++ oblig3/code/oblig3algoritmer/src/oblig3algoritmer/Oblig3algoritmer.java
@@ -1,22 +1,53 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package oblig3algoritmer;
 
-/**
- *
- * @author andbergl
- */
-public class Oblig3algoritmer {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public class Oblig3algoritmer {
+    private static int n;
+
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.println("" + args[0]);
+        Scanner in = new Scanner(new File(args[0]));
+        
+        n = in.nextInt();
+
+        int[][] arrN = new int[n][n];
+        int x = 0;
+        int y = 0;
+        while (in.hasNextInt() && x < n) {
+            int tall = in.nextInt();
+            if (y == n) {
+                y = 0;
+                x++;
+                arrN[x][y] = tall;
+                y++;
+            } else {
+                arrN[x][y] = tall;
+                y++;
+            }
+
+        }
+        
+        printArray(arrN);
+    }
+    
+//    public static void arrayCopy(int[][] src, Node dest) {
+//        for (int k = 0; k < n; k++) {
+//            System.arraycopy(src[k], 0, dest.arr[k], 0, src[k].length);
+//        }
+//    }
+    
+    public static void printArray(int[][] arr) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
+        System.out.print("\n");
     }
     
 }
